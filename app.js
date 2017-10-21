d910ea845f97d064019a426a56ee18ea7bce579e
Amaresh <amareshbingumalla@gmail.com>
2017-10-21 13:30:25 -0700

    Add Routing Capability

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ui.router']);
 
-app.controller('testController', ['$scope', function($scope) {
-
-
+app.controller('rootController', ['$scope', '$state', function($scope, $state) {
     $scope.aVal = 10;
     $scope.bVal = 100;
-    
+}]);
+
+
+app.config(function($stateProvider) {
+    var loginState = {
+        name: 'login',
+        url: '/login',
+        template: '<h3>Login Page</h3>'
+    };
 
     
-}]);
+    $stateProvider.state(loginState);
+});
