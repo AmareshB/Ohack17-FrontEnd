a192ecd66e9af33ebbf2c9cf829de60a81ddfa20
Amaresh <amareshbingumalla@gmail.com>
2017-10-21 21:34:27 -0700

    Dashboard route on sign n button

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -1,11 +1,17 @@
 var app = angular.module('Login', []);
 
-app.controller('LoginController', ['$scope',
-    function($scope) {
+app.controller('LoginController', ['$scope', '$location',
+    function($scope, $location) {
 
         var init = function() {
             alert("in alert of login");
             console.log("in init fn");
+        }
+
+        $scope.submit = function() {
+            console.log("in submit fn");
+           $location.path( 'dashboard' );
+          //  $urlRouterProvider.when('/login', '/dashboard');
         }
 
         init();
