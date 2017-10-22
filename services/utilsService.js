41980a9508e16bad29f7e682a7b8f4ebb5219d12
Amaresh <amareshbingumalla@gmail.com>
2017-10-22 09:03:31 -0700

    util service adding removed code

--- services/utilsService.js
+++ services/utilsService.js
@@ -7,9 +7,9 @@ app.service('utilsService', function($http) {
         console.log("in servcies");
         //return $http.get('/util/categoryColors');
 
-      /*  $http.get('http://10.143.191.81:8082/').success(function(response) {
-            console.log(response);
-        });*/
+        /*  $http.get('http://10.143.191.81:8082/').success(function(response) {
+              console.log(response);
+          });*/
 
         var data = [{
             id: "sd1",
@@ -32,10 +32,17 @@ app.service('utilsService', function($http) {
 
     };
 
+    this.login = function(username, pwd) {
+        return $http.post('http://nmtsa.herokuapp.com/login', {
+            "username": username,
+            "password": pwd
+        }, {});
+    };
+
 
     // this.sendRequest=function()
-    // {
-       
+    // {    
+
     // }
 
 
